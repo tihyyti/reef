f55a9ac167109a2d2d3ca563eeb6e3673e624244
tihyyti <timo.hyyppa@helsinki.fi>
2025-02-02 21:33:32 +0200

    First commit

---
+++ script_1v.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+print("TensorFlow Version:", tf.__version__)
+print("NumPy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
